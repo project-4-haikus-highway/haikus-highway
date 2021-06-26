dff1be1cf181447d95ce77cd84123377ffd43573
barish90 <baris.kutsal@gmail.com>
2021-06-26 13:17:11 -0400

    added form component with api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import UserForm from './UserForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Haikus Highway</h1>
+      <UserForm />
     </div>
   );
 }
