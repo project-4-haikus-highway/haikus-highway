344f8ab77e48f0bcce143e7ca773314b1c557518
barish90 <baris.kutsal@gmail.com>
2021-06-26 13:30:09 -0400

    removed use effect

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function UserForm() {
     const [soundsLike, setSoundsLike] = useState([])
